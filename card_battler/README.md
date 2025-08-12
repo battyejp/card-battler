2913721188ddd07788f7ff6021c802c09885467c
John Battye <John.Battye@ipfin.co.uk>
2025-08-12 16:04:07 +0100

    initial setup

---
+++ card_battler/README.md
@@ -0,0 +1,3 @@
+# card_battler
+
+A new Flutter project.
---
+++ card_battler/android/app/src/main/kotlin/com/example/card_battler/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.card_battler
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
