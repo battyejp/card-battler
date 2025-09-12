eb6685413c703661cd7996ab40e5ead5f7fdf8de
John Battye <John.Battye@ipfin.co.uk>
2025-09-12 21:18:40 +0100

    feat: Simplify PlayerInfo component by consolidating repeated code for displaying player stats

--- lib/game/ui/components/player/player_info.dart
+++ lib/game/ui/components/player/player_info.dart
@@ -18,77 +18,47 @@ class PlayerInfo extends PositionComponent {
   void onLoad() {
     super.onLoad();
 
-    var background = RectangleComponent(
-      size: size,
-      paint: Paint()
-        ..color = _coordinator.isActive
-            ? Colors.blue.withValues(alpha: 0.3)
-            : Colors.transparent,
-    );
-
-    add(background);
-
-    //TODO tidy up, loads of repeated code
-    var comp1 = PositionComponent()
-      ..size = Vector2(size.x / 4, size.y)
-      ..position = Vector2(0, 0);
-
-    var comp2 = PositionComponent()
-      ..size = Vector2(size.x / 4, size.y)
-      ..position = Vector2(size.x / 4, 0);
-
-    var comp3 = PositionComponent()
-      ..size = Vector2(size.x / 4, size.y)
-      ..position = Vector2((size.x / 4) * 2, 0);
-
-    var comp4 = PositionComponent()
-      ..size = Vector2(size.x / 4, size.y)
-      ..position = Vector2((size.x / 4) * 3, 0);
-
-    var nameLabel = TextComponent(
-      text: _coordinator.name,
-      position: Vector2(comp1.size.x / 2, comp1.size.y / 2),
-      anchor: Anchor.center,
-      textRenderer: TextPaint(
-        style: const TextStyle(fontSize: 20, color: Colors.white),
+    add(
+      RectangleComponent(
+        size: size,
+        paint: Paint()
+          ..color = _coordinator.isActive
+              ? Colors.blue.withValues(alpha: 0.3)
+              : Colors.transparent,
       ),
     );
 
-    var healthLabel = TextComponent(
-      text: 'Health: ${_coordinator.health}/${_coordinator.maxHealth}',
-      position: Vector2(comp2.size.x / 2, comp2.size.y / 2),
-      anchor: Anchor.center,
-      textRenderer: TextPaint(
-        style: const TextStyle(fontSize: 20, color: Colors.white),
-      ),
-    );
+    final isSummary = _viewMode == PlayerInfoViewMode.summary;
+    final statData = isSummary
+        ? [
+            _coordinator.name,
+            'Health: ${_coordinator.health}/${_coordinator.maxHealth}',
+          ]
+        : [
+            _coordinator.name,
+            'Health: ${_coordinator.health}/${_coordinator.maxHealth}',
+            'Attack: ${_coordinator.attack}',
+            'Credits: ${_coordinator.credits}',
+          ];
 
-    var attackLabel = TextComponent(
-      text: 'Attack: ${_coordinator.attack}',
-      position: Vector2(comp3.size.x / 2, comp3.size.y / 2),
-      anchor: Anchor.center,
-      textRenderer: TextPaint(
-        style: const TextStyle(fontSize: 20, color: Colors.white),
-      ),
-    );
+    final statWidth = size.x / statData.length;
 
-    var creditsLabel = TextComponent(
-      text: 'Credits: ${_coordinator.credits}',
-      position: Vector2(comp4.size.x / 2, comp4.size.y / 2),
-      anchor: Anchor.center,
-      textRenderer: TextPaint(
-        style: const TextStyle(fontSize: 20, color: Colors.white),
-      ),
-    );
+    for (var i = 0; i < statData.length; i++) {
+      final comp = PositionComponent()
+        ..size = Vector2(statWidth, size.y)
+        ..position = Vector2(statWidth * i, 0);
 
-    comp1.add(nameLabel);
-    comp2.add(healthLabel);
-    comp3.add(attackLabel);
-    comp4.add(creditsLabel);
+      final label = TextComponent(
+        text: statData[i],
+        position: Vector2(statWidth / 2, size.y / 2),
+        anchor: Anchor.center,
+        textRenderer: TextPaint(
+          style: const TextStyle(fontSize: 20, color: Colors.white),
+        ),
+      );
 
-    add(comp1);
-    add(comp2);
-    add(comp3);
-    add(comp4);
+      comp.add(label);
+      add(comp);
+    }
   }
 }
