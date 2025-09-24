d7d20579c5b8efb314e818bb1e3ae491be4964ac
John Battye <John.Battye@ipfin.co.uk>
2025-09-24 22:44:01 +0100

    refactor: Replace old ShopCard and ShopDisplay implementations with updated versions for improved functionality and layout

--- lib/game/ui/components/shop/shop_card.dart
+++ lib/game/ui/components/shop/shop_card.dart
@@ -1,9 +1,29 @@
 import 'package:card_battler/game/coordinators/components/shop/shop_card_coordinator.dart';
 import 'package:card_battler/game/ui/components/card/card_sprite.dart';
+import 'package:flame/components.dart';
+import 'package:flutter/material.dart';
 
 class ShopCard extends CardSprite {
   ShopCard(ShopCardCoordinator super.coordinator, super.isMini)
     : _coordinator = coordinator;
 
   final ShopCardCoordinator _coordinator;
+
+  @override
+  void onLoad() {
+    super.onLoad();
+
+    if (_coordinator.isFaceUp) {
+      final costText = TextComponent(
+        text: '\$${_coordinator.cost}',
+        position: Vector2(sprite!.srcSize.x / 2, 20),
+        anchor: Anchor.center,
+        textRenderer: TextPaint(
+          style: const TextStyle(fontSize: 36, color: Color(0xFFFFD700)),
+        ),
+      );
+
+      add(costText);
+    }
+  }
 }
