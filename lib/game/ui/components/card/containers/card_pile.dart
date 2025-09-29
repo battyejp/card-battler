7a2b7f1bac9a244c143c9e16f8868a0714546380
JP Battye <battyejp@gmail.com>
2025-09-29 22:15:54 +0100

    refactor: Clean up CardPile component; improve formatting and add empty indicator with card count

--- lib/game/ui/components/card/containers/card_pile.dart
+++ lib/game/ui/components/card/containers/card_pile.dart
@@ -1,13 +1,15 @@
 import 'package:card_battler/game/coordinators/components/cards/card_list_coordinator.dart';
+import 'package:card_battler/game/game_variables.dart';
 import 'package:card_battler/game/ui/components/card/card_sprite.dart';
 import 'package:card_battler/game/ui/components/common/reactive_position_component.dart';
 import 'package:flame/components.dart';
+import 'package:flutter/material.dart';
 
-//TODO do we want empty indicator or card count like old version, perhaps number in middle of top card?
 class CardPile extends ReactivePositionComponent<CardListCoordinator> {
   CardPile(
     super.coordinator, {
-    required bool isMini, bool showTopCard = false,
+    required bool isMini,
+    bool showTopCard = false,
     double scale = 1.0,
   }) : _showTopCard = showTopCard,
        _scale = scale,
@@ -23,13 +25,52 @@ class CardPile extends ReactivePositionComponent<CardListCoordinator> {
 
     for (var i = 0; i < coordinator.cardCoordinators.length; i++) {
       final cardCoordinator = coordinator.cardCoordinators[i];
-      final cardSprite =
-          CardSprite(cardCoordinator, _isMini)
-            ..position = Vector2(-i * 1.0 + size.x / 2, -i * 1.0 + size.y / 2)
-            ..anchor = Anchor.center
-            ..scale = Vector2.all(_scale);
+      final cardSprite = CardSprite(cardCoordinator, _isMini)
+        ..position = Vector2(-i * 1.0 + size.x / 2, -i * 1.0 + size.y / 2)
+        ..anchor = Anchor.center
+        ..scale = Vector2.all(_scale);
       cardSprite.coordinator.isFaceUp = _showTopCard;
       add(cardSprite);
     }
+
+    if (coordinator.cardCoordinators.isEmpty) {
+      final emptyIndicator = RectangleComponent(
+        size: Vector2(
+          _isMini
+              ? GameVariables.defaultCardBackSizeWidth * _scale
+              : GameVariables.defaultCardSizeWidth * _scale,
+          _isMini
+              ? GameVariables.defaultCardBackSizeHeight * _scale
+              : GameVariables.defaultCardSizeHeight * _scale,
+        ),
+        position: Vector2(size.x / 2, size.y / 2),
+        anchor: Anchor.center,
+        paint: Paint()
+          ..color = const Color(0xFFAAAAAA)
+          ..style = PaintingStyle.stroke
+          ..strokeWidth = 2.0 * _scale,
+      );
+      add(emptyIndicator);
+    }
+
+    final counter = TextComponent(
+      text: coordinator.cardCoordinators.length.toString(),
+      position: Vector2(size.x / 2, size.y / 2),
+      anchor: Anchor.center,
+      textRenderer: TextPaint(
+        style: TextStyle(
+          fontSize: 24,
+          color: const Color(0xFFFFFFFF),
+          shadows: [
+            Shadow(
+              offset: Offset(1.5 * _scale, 1.5 * _scale),
+              blurRadius: 2.0 * _scale,
+            ),
+          ],
+        ),
+      ),
+    );
+
+    add(counter);
   }
 }
