b8afeafa4ed334bc48e45c8d10a9f5a808af9d44
JP Battye <battyejp@gmail.com>
2025-10-06 08:41:25 +0100

    add new cards

--- lib/game/game_variables.dart
+++ lib/game/game_variables.dart
@@ -1,10 +1,10 @@
 class GameVariables {
   static const numberOfCardsDrawnByPlayer = 5;
 
-  static const int defaultCardSizeWidth = 350;
+  static const int defaultCardSizeWidth = 329;
   static const int defaultCardSizeHeight = 560;
 
-  static const int defaultMiniCardSizeWidth = 31;
+  static const int defaultMiniCardSizeWidth = 35;
   static const int defaultMiniCardSizeHeight = 60;
 
   static const int defaultCardBackSizeWidth = 51;
